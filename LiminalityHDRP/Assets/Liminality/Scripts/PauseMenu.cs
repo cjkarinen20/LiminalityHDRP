eb882619c0e0b432d95aa71a2c73692d3bde8ccb
Cj Karinen <Cjkarinen20@gmail.com>
2024-08-18 23:10:56 -0400

    PauseMenu and Main Menu Functionality
    -Added functionality to buttons on main menu and pause menu
    -Added empty settings menu
    -Note: Still need to add a save system

--- LiminalityHDRP/Assets/Liminality/Scripts/PauseMenu.cs
+++ LiminalityHDRP/Assets/Liminality/Scripts/PauseMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -54,5 +55,18 @@ public class PauseMenu : MonoBehaviour
         playerController.mouseLookEnabled = true;
         Time.timeScale = 1f;
         isPaused = false;
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    public void MainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(0);
+    }
+    public void SettingsMenu()
+    {
+        
     }
 }
--- LiminalityHDRP/Assets/Liminality/Scripts/Water.cs
+++ LiminalityHDRP/Assets/Liminality/Scripts/Water.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Water : MonoBehaviour
 {
+
+    public NewFPSController playerController;
     // Start is called before the first frame update
     void Start()
     {
