6ed3f3076d6ac3ea94ef134880f16f37f93de8ad
Cj Karinen <Cjkarinen20@gmail.com>
2024-09-09 22:51:25 -0400

    AI Search Feature (untested)
    -Added some new features to the AI scripts but haven't tested them yet

--- LiminalityHDRP/Assets/Liminality/Scripts/AI/enemyAI.cs
+++ LiminalityHDRP/Assets/Liminality/Scripts/AI/enemyAI.cs
@@ -4,42 +4,57 @@ using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.SceneManagement;
 
-public class EnemyAI : MonoBehaviour
+public class enemyAI : MonoBehaviour
 {
     public NavMeshAgent ai;
     public List<Transform> destinations;
     public Animator aiAnim;
-    public float walkSpeed, chaseSpeed, minIdleTime, maxIdleTime, idleTime, sightDistance, catchDistance, chaseTime, minChaseTime, maxChaseTime, jumpscareTime;
-    public bool walking, chasing;
+    public float walkSpeed, chaseSpeed, minIdleTime, maxIdleTime, idleTime, detectionDistance, catchDistance, searchDistance, minChaseTime, maxChaseTime, minSearchTime, maxSearchTime, jumpscareTime;
+    public bool walking, chasing, searching;
     public Transform player;
     Transform currentDest;
     Vector3 dest;
-    int randNum;
-    public int destinationAmount;
     public Vector3 rayCastOffset;
     public string deathScene;
+    public float aiDistance;
+    public GameObject hideText, stopHideText;
 
     void Start()
     {
         walking = true;
-        randNum = Random.Range(0, destinations.Count);
-        currentDest = destinations[randNum];
-        aiAnim = GetComponent<Animator>();
+        currentDest = destinations[Random.Range(0, destinations.Count)];
     }
     void Update()
     {
-        Debug.Log(currentDest);
         Vector3 direction = (player.position - transform.position).normalized;
         RaycastHit hit;
-        if (Physics.Raycast(transform.position + rayCastOffset, direction, out hit, sightDistance))
+        aiDistance = Vector3.Distance(player.position, this.transform.position);
+        if (Physics.Raycast(transform.position + rayCastOffset, direction, out hit, detectionDistance))
         {
             if (hit.collider.gameObject.tag == "Player")
             {
                 walking = false;
                 StopCoroutine("stayIdle");
+                StopCoroutine("searchRoutine");
+                StartCoroutine("searchRoutine");
+                searching = true;
+            }
+        }
+        if (searching == true)
+        {
+            ai.speed = 0;
+            aiAnim.ResetTrigger("walk");
+            aiAnim.ResetTrigger("idle");
+            aiAnim.ResetTrigger("sprint");
+            aiAnim.SetTrigger("search");
+            if (aiDistance <= searchDistance)
+            {
+                StopCoroutine("stayIdle");
+                StopCoroutine("searchRoutine");
                 StopCoroutine("chaseRoutine");
                 StartCoroutine("chaseRoutine");
                 chasing = true;
+                searching = false;
             }
         }
         if (chasing == true)
@@ -47,21 +62,22 @@ public class EnemyAI : MonoBehaviour
             dest = player.position;
             ai.destination = dest;
             ai.speed = chaseSpeed;
+            aiAnim.ResetTrigger("walk");
             aiAnim.ResetTrigger("idle");
-            aiAnim.SetTrigger("walk");
-            float distance = Vector3.Distance(player.position, ai.transform.position);
-            if (distance <= catchDistance)
+            aiAnim.ResetTrigger("search");
+            aiAnim.SetTrigger("sprint");
+            if (aiDistance <= catchDistance)
             {
                 player.gameObject.SetActive(false);
                 aiAnim.ResetTrigger("walk");
                 aiAnim.ResetTrigger("idle");
+                aiAnim.ResetTrigger("search");
+                hideText.SetActive(false);
+                stopHideText.SetActive(false);
+                aiAnim.ResetTrigger("sprint");
                 aiAnim.SetTrigger("jumpscare");
-                if (Physics.Raycast(transform.position + rayCastOffset, direction, out hit, catchDistance))
-                {
-                    StartCoroutine(damageRoutine());
-                    chasing = false;
-                }
-
+                StartCoroutine(deathRoutine());
+                chasing = false;
             }
         }
         if (walking == true)
@@ -69,11 +85,15 @@ public class EnemyAI : MonoBehaviour
             dest = currentDest.position;
             ai.destination = dest;
             ai.speed = walkSpeed;
+            aiAnim.ResetTrigger("sprint");
             aiAnim.ResetTrigger("idle");
+            aiAnim.ResetTrigger("search");
             aiAnim.SetTrigger("walk");
             if (ai.remainingDistance <= ai.stoppingDistance)
             {
+                aiAnim.ResetTrigger("sprint");
                 aiAnim.ResetTrigger("walk");
+                aiAnim.ResetTrigger("search");
                 aiAnim.SetTrigger("idle");
                 ai.speed = 0;
                 StopCoroutine("stayIdle");
@@ -82,27 +102,35 @@ public class EnemyAI : MonoBehaviour
             }
         }
     }
+    public void stopChase()
+    {
+        walking = true;
+        chasing = false;
+        StopCoroutine("chaseRoutine");
+        currentDest = destinations[Random.Range(0, destinations.Count)];
+    }
     IEnumerator stayIdle()
     {
         idleTime = Random.Range(minIdleTime, maxIdleTime);
         yield return new WaitForSeconds(idleTime);
         walking = true;
-        randNum = Random.Range(0, destinations.Count);
-        currentDest = destinations[randNum];
+        currentDest = destinations[Random.Range(0, destinations.Count)];
+    }
+    IEnumerator searchRoutine()
+    {
+        yield return new WaitForSeconds(Random.Range(minSearchTime, maxSearchTime));
+        searching = false;
+        walking = true;
+        currentDest = destinations[Random.Range(0, destinations.Count)];
     }
     IEnumerator chaseRoutine()
     {
-        chaseTime = Random.Range(minChaseTime, maxChaseTime);
-        yield return new WaitForSeconds(chaseTime);
-        walking = true;
-        chasing = false;
-        randNum = Random.Range(0, destinations.Count);
-        currentDest = destinations[randNum];
+        yield return new WaitForSeconds(Random.Range(minChaseTime, maxChaseTime));
+        stopChase();
     }
-    IEnumerator damageRoutine()
+    IEnumerator deathRoutine()
     {
         yield return new WaitForSeconds(jumpscareTime);
-        NewFPSController.OnTakeDamage(15);
-
+        SceneManager.LoadScene(deathScene);
     }
 }
--- LiminalityHDRP/Assets/Liminality/Scripts/AI/hidingPlace.cs
+++ LiminalityHDRP/Assets/Liminality/Scripts/AI/hidingPlace.cs
@@ -6,10 +6,12 @@ public class hidingPlace : MonoBehaviour
 {
     public GameObject hideText, stopHideText;
     public GameObject normalPlayer, hidingPlayer;
-    public EnemyAI monsterScript;
+    public enemyAI monsterScript;
     public Transform monsterTransform;
     bool interactable, hiding;
     public float loseDistance;
+    public AudioSource hideSound, stopHideSound;
+    public roomDetector detector;
 
     void Start()
     {
@@ -20,8 +22,16 @@ public class hidingPlace : MonoBehaviour
     {
         if (other.CompareTag("MainCamera"))
         {
-            hideText.SetActive(true);
-            interactable = true;
+            if (detector.inTrigger == true)
+            {
+                hideText.SetActive(true);
+                interactable = true;
+            }
+            else if (detector.inTrigger == false)
+            {
+                hideText.SetActive(false);
+                interactable = false;
+            }
         }
     }
     void OnTriggerExit(Collider other)
@@ -39,13 +49,14 @@ public class hidingPlace : MonoBehaviour
             if (Input.GetKeyDown(KeyCode.E))
             {
                 hideText.SetActive(false);
+                hideSound.Play();
                 hidingPlayer.SetActive(true);
                 float distance = Vector3.Distance(monsterTransform.position, normalPlayer.transform.position);
                 if (distance > loseDistance)
                 {
                     if (monsterScript.chasing == true)
                     {
-                        monsterScript.chasing = false;
+                        monsterScript.stopChase();
                     }
                 }
                 stopHideText.SetActive(true);
@@ -59,6 +70,7 @@ public class hidingPlace : MonoBehaviour
             if (Input.GetKeyDown(KeyCode.Q))
             {
                 stopHideText.SetActive(false);
+                stopHideSound.Play();
                 normalPlayer.SetActive(true);
                 hidingPlayer.SetActive(false);
                 hiding = false;
