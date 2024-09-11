132667c1f40fb91d746fb0e9a11e437d55ff1b56
Cj Karinen <42248595+cjkarinen20@users.noreply.github.com>
2024-09-11 11:48:10 -0400

    Simple "Weeping Angel AI"
    -Converted the first test scene to test a ghost enemy
    -Functionally similar to the "Weeping Angels" from Doctor Who
    -Currently, it does not kill or harm the player
    -Note: If continue forward with the having this entity be nonhostile then I would like to specify a stalking distance so that it never gets too close to the player, instead following them from a distance.

---
+++ LiminalityHDRP/Assets/Liminality/Scripts/AI/stalkerAI.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class stalkerAI : MonoBehaviour
+{
+    public NavMeshAgent stalker;
+    public Transform player;
+    Vector3 destination;
+    public Camera playerCam;
+    public float aiSpeed;
+
+    private void Update()
+    {
+        Plane[] planes = GeometryUtility.CalculateFrustumPlanes(playerCam);
+
+        if (GeometryUtility.TestPlanesAABB(planes, this.gameObject.GetComponent<Renderer>().bounds))
+        {
+            stalker.speed = 0;
+            stalker.SetDestination(transform.position);
+        }
+
+        if (!GeometryUtility.TestPlanesAABB(planes, this.gameObject.GetComponent<Renderer>().bounds))
+        {
+            stalker.speed = aiSpeed;
+            destination = player.position;
+            stalker.destination = destination;
+        }
+    }
+
+
+
+
+}
